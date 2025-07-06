5bcedb14d39197d234be70375c7278942427bdc4
jk <jaka.klinar1@gmail.com>
2025-07-06 17:07:48 +0200

    updates

--- bpe.c
+++ bpe.c
@@ -72,7 +72,7 @@ kafka.txt : 5288 bytes -> 1538 bytes
 int main(){
     // const char* txt = "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text.";
     // const char* txt = "aaabdaaabac";
-    FILE* input = fopen("samsa.txt", "r");
+    FILE* input = fopen("kafka.txt", "r");
     char* txt = calloc(10010, sizeof(char));
     fgets(txt, 10010, input);
     int textSize = strlen(txt);
